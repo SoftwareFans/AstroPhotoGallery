a4f75ed445fd98ff4784b9b32dc3621fa989e928
stefkavasileva <stefkavasileva93@gmail.com>
2017-03-11 15:03:47 +0200

    Initial commit

---
+++ AstroPhotoGallery/AstroPhotoGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AstroPhotoGallery.Startup))]
+namespace AstroPhotoGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
