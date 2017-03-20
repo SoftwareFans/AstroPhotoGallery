73a9eab87845fd7a4cf0bfcf98db3bed769fdcf2
stefkavasileva <stefkavasileva93@gmail.com>
2017-03-20 17:00:53 +0200

    Add controller and remove some empty comments

---
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/PictureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AstroPhotoGallery.Controllers
+{
+    public class PictureController : Controller
+    {
+        // GET: Picture
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AstroPhotoGallery/AstroPhotoGallery/Models/Picture.cs
+++ AstroPhotoGallery/AstroPhotoGallery/Models/Picture.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace AstroPhotoGallery.Models
 {
-    public class Image
+    public class Picture
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PicTitle { get; set; }
+
+        [Required]
+        public string PicDescription { get; set; }
+
+        [ForeignKey("Uploader")]
+        public string PicUploaderId { get; set; }
+
+        public virtual ApplicationUser PicUploader { get; set; }
     }
 }
