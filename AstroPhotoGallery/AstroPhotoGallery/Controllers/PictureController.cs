be9e76d069729d81fe51332d9669a89717995ec5
stefkavasileva <stefkavasileva93@gmail.com>
2017-03-25 15:51:11 +0200

    Add some test pics, add Details view and action to PictureController

--- AstroPhotoGallery/AstroPhotoGallery/Controllers/PictureController.cs
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/PictureController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AstroPhotoGallery.Models;
@@ -16,7 +17,7 @@ namespace AstroPhotoGallery.Controllers
             return RedirectToAction("List");
         }
 
-        //GET:Picture/List
+        //GET: Picture/List
         public ActionResult List()
         {
             using (var bd = new GalleryDbContext())
@@ -28,6 +29,29 @@ namespace AstroPhotoGallery.Controllers
                     .ToList();
 
                 return View(pictures);
+            }
+        }
+
+        //GET: Picture/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var db = new GalleryDbContext())
+            {
+                  //Get picture from batabase
+
+                var picture = db.Pictures.Where(x => x.Id == id).Include(u => u.PicUploader).First();
+
+                if (picture == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(picture);
             }
         }
     }
