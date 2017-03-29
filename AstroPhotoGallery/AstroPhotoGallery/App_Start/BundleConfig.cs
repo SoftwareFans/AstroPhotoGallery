82989c7db5b74cdac65ba5580c21ce028c1e9931
stefkavasileva <stefkavasileva93@gmail.com>
2017-03-29 19:05:54 +0300

    Add css and JS file for the index page

--- AstroPhotoGallery/AstroPhotoGallery/App_Start/BundleConfig.cs
+++ AstroPhotoGallery/AstroPhotoGallery/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace AstroPhotoGallery
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));           
-
+           
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
