8d7e7aa4346d1a32a93faa6a206b26f39e7e3438
stefkavasileva <stefkavasileva93@gmail.com>
2017-04-02 12:29:59 +0300

    Add Create - post and get methods. Add view for create category

---
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AstroPhotoGallery.Models;
+
+namespace AstroPhotoGallery.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //GET: Category/List
+        public ActionResult List()
+        {
+            using (var db = new GalleryDbContext())
+            {
+                var categories = db.Categories.ToList();
+
+                return View(categories);
+            }
+        }
+
+        //GET: Categori/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //If modelstate is valid add category to categories in database and redirect to view with all categories
+                using (var db = new GalleryDbContext())
+                {
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            //If modelstate is not valid render the same view with category
+            return View(category);
+        }
+    }
+}
