9dc2cf421ddfcd404a37b75510e143d27b622571
stefkavasileva <stefkavasileva93@gmail.com>
2017-04-02 12:45:36 +0300

    Add Edit - post and get methods. Add view for edit category

--- AstroPhotoGallery/AstroPhotoGallery/Controllers/CategoryController.cs
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AstroPhotoGallery.Models;
@@ -49,6 +51,48 @@ namespace AstroPhotoGallery.Controllers
             }
 
             //If modelstate is not valid render the same view with category
+            return View(category);
+        }
+
+        //GET: Category/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var db = new GalleryDbContext())
+            {
+                //Get category form database if id is not null
+                var category = db.Categories.FirstOrDefault(c => c.Id == id);
+
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
+
+                //Return category, if is not null
+                return View(category);
+            }
+        }
+
+        //POST: Category/Edit
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new GalleryDbContext())
+                {
+                    //Edit category if model state is valid and redirect to index
+                    db.Entry(category).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
             return View(category);
         }
     }
