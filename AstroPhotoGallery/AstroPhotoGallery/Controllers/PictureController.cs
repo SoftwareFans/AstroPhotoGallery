6570f62a902b6727ce534a0ee9b340850e6bd6e1
gaydov <gaidov@gmail.com>
2017-04-04 23:16:55 +0300

    added picture validation basde on file headers, add feature when the picture is deleted from DB to be deleted from /Content/images too, add validations in case of errors

--- AstroPhotoGallery/AstroPhotoGallery/Controllers/PictureController.cs
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/PictureController.cs
@@ -15,7 +15,7 @@ namespace AstroPhotoGallery.Controllers
 
         private bool IsUserAuthorizedToEdit(Picture picture)
         {
-            //chech for admin
+            // Check is the user is the uploader
             bool isUploader = picture.IsUploader(this.User.Identity.Name);
             return isUploader;
         }
@@ -50,9 +50,9 @@ namespace AstroPhotoGallery.Controllers
 
             using (var db = new GalleryDbContext())
             {
-                //Get picture from batabase
+                //Get picture from database
 
-                var picture = db.Pictures.Where(x => x.Id == id).Include(u => u.PicUploader).First();
+                var picture = db.Pictures.Where(x => x.Id == id).Include(u => u.PicUploader).FirstOrDefault();
 
                 if (picture == null)
                 {
@@ -93,29 +93,47 @@ namespace AstroPhotoGallery.Controllers
 
                     //Set picture uploader
                     picture.PicUploaderId = uploaderId;
-                    //Pic validation
 
-                    if (Request.Files.Count > 0)
+                    if (Request.Files.Count > 0 && Request.Files[0].ContentLength > 0)
                     {
-
                         var poImgFile = Request.Files["ImagePath"];
                         var pic = Path.GetFileName(poImgFile.FileName);
+
                         var path = Path.Combine(Server.MapPath("~/Content/images"), pic);
-
                         poImgFile.SaveAs(path);
-                        picture.ImagePath = "~/Content/images/" + pic; 
-                       
+
+                        if (IsValidImage(path))
+                        {
+                            picture.ImagePath = "~/Content/images/" + pic;
+
+                            // Getting the name of the category to add it to the current picture's property:
+                            var picCategoryName = db.Categories.Where(c => c.Id == picture.CategoryId).Select(c => c.Name).ToArray();
+                            picture.CategoryName = picCategoryName[0];
+
+                            //Save pic in database
+                            db.Pictures.Add(picture);
+                            db.SaveChanges();
+
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            // Deleting the file from ~/Content/images:
+                            System.IO.File.Delete(path);
+
+                            throw new Exception("Invalid picture format!");
+                        }
                     }
-
-                    //Save pic in database
-                    db.Pictures.Add(picture);
-                    db.SaveChanges();
-
-                    return RedirectToAction("Index");
+                    else
+                    {
+                        throw new Exception("No picture selected for upload.");
+                    }
                 }
             }
-
-            return View(picture);
+            else
+            {
+                throw new Exception("Missing title or description.");
+            }
         }
 
         //GET: Picture/Delete
@@ -132,7 +150,7 @@ namespace AstroPhotoGallery.Controllers
                 var picture = db.Pictures
                     .Where(p => p.Id == id)
                     .Include(p => p.PicUploader)
-                    .First();              
+                    .FirstOrDefault();
 
                 if (!IsUserAuthorizedToEdit(picture))
                 {
@@ -143,7 +161,7 @@ namespace AstroPhotoGallery.Controllers
                 {
                     return HttpNotFound();
                 }
-                //Pass picture to  view
+                //Pass picture to view
 
                 return View(picture);
             }
@@ -165,7 +183,7 @@ namespace AstroPhotoGallery.Controllers
                 var picture = db.Pictures
                     .Where(p => p.Id == id)
                     .Include(p => p.PicUploader)
-                    .First();
+                    .FirstOrDefault();
 
                 //Check if picture exists
                 if (picture == null)
@@ -173,9 +191,14 @@ namespace AstroPhotoGallery.Controllers
                     return HttpNotFound();
                 }
 
-                //Delete article from database 
+                //Delete the picture from the database 
+                var picPath = picture.ImagePath;
                 db.Pictures.Remove(picture);
                 db.SaveChanges();
+
+                //Delete the picture from the ~/Content/images folder:
+                var mappedPath = Server.MapPath(picPath);
+                System.IO.File.Delete(mappedPath);
 
                 //Redirect to index page
                 return RedirectToAction("Index");
@@ -195,7 +218,7 @@ namespace AstroPhotoGallery.Controllers
                 //Get picture from database
                 var picture = db.Pictures
                     .Where(p => p.Id == id)
-                    .First();
+                    .FirstOrDefault();
 
                 if (!IsUserAuthorizedToEdit(picture))
                 {
@@ -215,6 +238,7 @@ namespace AstroPhotoGallery.Controllers
                 model.PicDescription = picture.PicDescription;
                 model.ImagePath = picture.ImagePath;
                 model.CategoryId = picture.CategoryId;
+                model.CategoryName = picture.CategoryName;
                 model.Categories = db.Categories.OrderBy(c => c.Name).ToList();
 
                 //Pass the view model to view
@@ -238,6 +262,8 @@ namespace AstroPhotoGallery.Controllers
                     picture.PicTitle = model.PicTitle;
                     picture.PicDescription = model.PicDescription;
                     picture.CategoryId = model.CategoryId;
+                    var picCategoryName = db.Categories.Where(c => c.Id == model.CategoryId).Select(c => c.Name).ToArray();
+                    picture.CategoryName = picCategoryName[0];
 
                     //Save pic state in database
                     db.Entry(picture).State = EntityState.Modified;
@@ -247,11 +273,98 @@ namespace AstroPhotoGallery.Controllers
                     return RedirectToAction("Index");
                 }
             }
-
-            //If model state is invalid,return the same view
-            return View(model);
+            else
+            {
+                throw new Exception("Missing title or description.");
+            }
         }
 
+        // Primary method for checking if the image is in valid format:
+        public static bool IsValidImage(string filePath)
+        {
+            return System.IO.File.Exists(filePath) &&
+                   IsValidStream(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+        }
 
+        // Method for IsValidImage - checking the data passed to the stream:
+        public static bool IsValidStream(FileStream inputStream)
+        {
+            using (inputStream)
+            {
+                Dictionary<string, List<byte[]>> imageHeaders = new Dictionary<string, List<byte[]>>();
+
+                // For every image type there is a list of byte arrays with its headers possible values.
+                // Other image types can be added in case of need - TIF, GIF etc.
+
+                imageHeaders.Add("JPG", new List<byte[]>());
+                imageHeaders.Add("JPEG", new List<byte[]>());
+                imageHeaders.Add("PNG", new List<byte[]>());
+                imageHeaders.Add("BMP", new List<byte[]>());
+                imageHeaders.Add("ICO", new List<byte[]>());
+
+                imageHeaders["JPG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 });
+                imageHeaders["JPG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 });
+                imageHeaders["JPG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xFE });
+                imageHeaders["JPEG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 });
+                imageHeaders["JPEG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 });
+                imageHeaders["JPEG"].Add(new byte[] { 0xFF, 0xD8, 0xFF, 0xFE });
+                imageHeaders["PNG"].Add(new byte[] { 0x89, 0x50, 0x4E, 0x47 });
+                imageHeaders["BMP"].Add(new byte[] { 0x42, 0x4D });
+                imageHeaders["ICO"].Add(new byte[] { 0x00, 0x00, 0x01, 0x00 });
+
+                if (inputStream.Length > 0) // If the file has any data at all
+                {
+                    string fileExt = inputStream.Name.Substring(inputStream.Name.LastIndexOf('.') + 1).ToUpper();
+
+                    if (!(fileExt.Equals("JPG") ||
+                        fileExt.Equals("JPEG") ||
+                        fileExt.Equals("PNG") ||
+                        fileExt.Equals("BMP") ||
+                        fileExt.Equals("ICO")
+                        ))
+                    {
+                        return false;
+                    }
+
+                    foreach (var subType in imageHeaders[fileExt])
+                    {
+                        byte[] standardHeader = subType;
+                        byte[] checkedHeader = new byte[standardHeader.Length];
+
+                        inputStream.Read(checkedHeader, 0, checkedHeader.Length);
+
+                        if (CompareArrays(standardHeader, checkedHeader))
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+                else
+                {
+                    throw new Exception("Empty file chosen.");
+                }
+            }
+        }
+
+        // Method for IsValidImage - comparing two byte arrays:
+        public static bool CompareArrays(byte[] firstArr, byte[] secondArr)
+        {
+            if (firstArr.Length != secondArr.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (firstArr[i] != secondArr[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
