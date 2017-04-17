b08785068117b8e7b6ab27ca9825b2657758fe17
gaydov <gaidov@gmail.com>
2017-04-17 17:07:24 +0300

    add pictureviewmodel due to issues with DB if there is only picture model

---
+++ AstroPhotoGallery/AstroPhotoGallery/Models/PictureViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AstroPhotoGallery.Models
+{
+    public class PictureViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
+        public string PicTitle { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string PicDescription { get; set; }
+
+        public string PicUploaderId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
