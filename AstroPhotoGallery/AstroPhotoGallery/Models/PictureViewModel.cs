d15bfb525b095f6f86dbcd57feb2d66c3e8c77fe
gaydov <gaidov@gmail.com>
2017-04-19 22:02:47 +0300

    added text length limit for the description, changed display names

--- AstroPhotoGallery/AstroPhotoGallery/Models/PictureViewModel.cs
+++ AstroPhotoGallery/AstroPhotoGallery/Models/PictureViewModel.cs
@@ -17,17 +17,21 @@ namespace AstroPhotoGallery.Models
         public string PicTitle { get; set; }
 
         [Required]
+        [StringLength(1300)]
         [DisplayName("Description")]
         public string PicDescription { get; set; }
 
         public string PicUploaderId { get; set; }
 
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
 
         public ICollection<Category> Categories { get; set; }
 
         public string ImagePath { get; set; }
 
+        [Required]
+        [DisplayName("Tags separated by \", \"")]
         public string Tags { get; set; }
     }
 }
