7751c4d1dcecb0dd02236043e21e31f004336116
gaydov <gaidov@gmail.com>
2017-04-20 22:52:09 +0300

    implemented default redirect when a non-existing URL is entered - error 404

---
+++ AstroPhotoGallery/AstroPhotoGallery/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AstroPhotoGallery.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View("Error404");
+        }
+    }
+}
