03d631558bd28971c8996bb7866f6861eda4f8f7
stefkavasileva <stefkavasileva93@gmail.com>
2019-02-13 10:34:29 +0200

    Resolve versions conflicts, remove DataAnnotations from db models

--- AstroPhotoGallery/AstroPhotoGallery.Models/ApplicationUser.cs
+++ AstroPhotoGallery/AstroPhotoGallery.Models/ApplicationUser.cs
@@ -11,12 +11,8 @@ namespace AstroPhotoGallery.Models
 
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [DisplayName("First name")]
         public string FirstName { get; set; }
 
-        [Required]
-        [DisplayName("Last name")]
         public string LastName { get; set; }
 
         public string Gender { get; set; }
@@ -25,15 +21,13 @@ namespace AstroPhotoGallery.Models
 
         public string City { get; set; }
 
-        // property PhoneNumber is part of IdentityUser by default: 
-        // IdentityUser<TKey, TLogin, TRole, TClaim> - public virtual string PhoneNumber { get; set; }
-
         public string Birthday { get; set; } = string.Empty; // default value when creating a user in order not to be null
 
         public string ImagePath { get; set; }
 
         public bool IsEmailPublic { get; set; } = false; // setting the default to be false due to privacy reasons
 
+        //TODO: difrent project?
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
